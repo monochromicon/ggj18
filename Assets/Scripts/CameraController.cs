94e6def34f2fda1b760f44c35c920b28b302ac05
Graham Barber <grahammbarber@gmail.com>
2018-01-26 21:24:02 -0800

    Add movement and camera following behaviours

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+	private float initialY;
+
+    void Start () {
+		// Get the initial Y of the camera
+		initialY = transform.position.y;
+        // Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset =  new Vector3(transform.position.x, 0f, transform.position.z) - new Vector3(player.transform.position.x, 0f, player.transform.position.z);
+    }
+    
+    void LateUpdate () {
+		Vector3 newLoc = player.transform.position + offset;
+		// Set new camera to follow only on X and Z
+        transform.position = new Vector3(newLoc.x, initialY, newLoc.z);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+
+        Vector3 movement = new Vector2 (moveHorizontal, 0.0f);
+
+        rb.AddForce (movement * speed);
+    }
+}
