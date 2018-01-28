9af8e920c3f7f268f140d9cc051c928d3bb7a501
Graham Barber <grahammbarber@gmail.com>
2018-01-27 17:15:55 -0800

    Add particle systems for digging and broadcasting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,38 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public ParticleSystem digSystem;
+    public ParticleSystem broadcastSystem;
 
     private Rigidbody rb;
+    private bool isBroadcasting = false;
+    private bool isDigging = false;
 
     void Start () {
         rb = GetComponent<Rigidbody>();
+    }
+
+    void Update () {
+        if (!digSystem.isEmitting) { isDigging = false; }
+        if (!broadcastSystem.isEmitting) { isBroadcasting = false; }
+
+        if (!isBroadcasting) {
+            if (Input.GetKeyDown(KeyCode.UpArrow)) {
+                broadcastSystem.Play();
+                isBroadcasting = true;
+            }
+        }
+
+        if (!isDigging) {
+            if (Input.GetKeyDown(KeyCode.DownArrow)) {
+                digSystem.Play();
+                isDigging = true;
+            }
+        }
+    }
+
+    public void Dig () {
+
     }
 
     void FixedUpdate () {
---
+++ Assets/Scripts/PlayerParticleController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerParticleController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start () {
+        // Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - player.transform.position;
+    }
+    
+    void LateUpdate () {
+        transform.position = player.transform.position + offset;
+    }
+}
